d87a11aadb7b09c62ab1c69aaff519ea5ac89ade
Omar <omarsefo7@gmail.com>
2023-08-24 18:46:26 +0300

    new update

--- mjs.js
+++ mjs.js
@@ -1,13 +1,29 @@
+setTimeout(function() {
+    var response = confirm("Do you want to see the real UI/UX images that I copied?\nClick OK to allow pop-ups for this site and reload the page please.");
+    if (response) {
+        var image = new Image();
+        image.src = "./img/img.jpg";
+        image.alt = "ice cream";
+
+        var newWindow = window.open("", "_blank");
+        
+        newWindow.document.write("<!DOCTYPE html><html><head><title>Image Display</title></head><body>");
+        newWindow.document.write("<img width='100%' src='" + image.src + "' alt='" + image.alt + "'>");
+        newWindow.document.write("</body></html>");
+    } else {
+
+    }
+  }, 1500);
+
 function imgc(bb){
     document.getElementById("star").src = bb;
 }
-function chang(color){
+function chang(color, acolor){
     var circl1 =document.querySelector('.back');
     var circl2 =document.querySelector('.back2');
     circl1.style.background=color;
     circl2.style.background=color;
-    h2.style.color=color;
-    a.style.background=color;
+    a.style.background=acolor;
 }
 var span =document.querySelector('span');
 var h2 =document.getElementById('h2');
@@ -18,21 +34,21 @@ var img1 = document.getElementById("img1");
 var img2 = document.getElementById("img2");
 
 img1.addEventListener('click',()=>{
-    h2.innerHTML="Caramel";
+    h2.innerHTML="Blueberry";
     star1.style.color="#111";
-    span.innerHTML="$3.50";
+    span.innerHTML="$2.50";
     h4.innerHTML="40.5 K";
 });
 img2.addEventListener('click',()=>{
     h2.innerHTML="Chocolate";
-    star1.style.color="#444";
-    span.innerHTML="$2.50";
+    star1.style.color="#44444496";
+    span.innerHTML="$3.50";
     h4.innerHTML="33.2 K";
 });
 a.addEventListener('click',()=>{
-    document.getElementById("star").src="inng/photo_2jpg.jpg";
+    document.getElementById("star").src="img/photo_2jpg.jpg";
     setTimeout(()=>{
-        document.getElementById("star").src="inng/caramel1.png";
+        document.getElementById("star").src="img/bluebery.png";
     },5000)
 })
 
